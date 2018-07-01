9b0958f30147d0382f5ea3b4e19a88259d60be57
Shimbo Takaaki <ta7uwtaka@gmail.com>
2018-07-01 23:46:16 +0900

    Create: local_settings.py for secret key

--- Similar_Image_Search_App/settings.py
+++ Similar_Image_Search_App/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-
+from .local_settings import LOCAL_SECRET_KEY
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8)$_1y#9arwpv8tl3h0gkmq5h6&rvzxg%5-owzqnu^90ws#*pb'
+
+SECRET_KEY = LOCAL_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "sisApp",
+
 ]
 
 MIDDLEWARE = [
--- sisApp/models.py
+++ sisApp/models.py
@@ -7,4 +7,6 @@ from django.db import models
 class Item(models.Model):
     name = models.CharField(max_length=128)
     image = models.ImageField()
+   
+
 
