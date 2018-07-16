da3fef7a7c0b50ff990e0abda590c16c63ff473b
Shimbo Takaaki <ta7uwtaka@gmail.com>
2018-07-16 23:08:26 +0900

    Add: media file path in urls.py

--- sisApp/urls.py
+++ sisApp/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import TopPage
+from settings import settings
 
 
 urlpatterns = [
     path("", TopPage.as_view()),
+    # TODO: media file path
+    path("media/", "django.views.static.serve", {"docoument_root": settings.MEDIA_ROOT})
 ]
