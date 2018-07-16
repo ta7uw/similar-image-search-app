16f75b5408110491522cccd1651a5bd20c52187c
Shimbo Takaaki <ta7uwtaka@gmail.com>
2018-07-16 23:36:38 +0900

    Implement: decorator delete_previous_file

--- sisApp/models.py
+++ sisApp/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from uuid import uuid4
 import os
 
+from settings import settings
+
 
 class Item(models.Model):
     class Meta:
@@ -13,6 +15,18 @@ class Item(models.Model):
     def __str__(self):
         return self.name
 
+    def get_image_path(filename):
+        """
+        To Get new customized file path
+        :param filename: original file name
+        :return: customized file name
+        """
+        prefix = "images/"
+        file_type = "origin/"
+        name = str(uuid4()).replace("-", "")
+        extension = os.path.splitext(filename)[-1]
+        return prefix + file_type + name + extension
+
 
 class UploadedImage(models.Model):
 
@@ -21,16 +35,54 @@ class UploadedImage(models.Model):
 
     image = models.ImageField(verbose_name="uploaded image", upload_to="uploaded")
 
+    def delete_previous_file(function):
+        """
 
-def get_image_path(file_type, filename):
-    """
-    To Get new customized file path
-    :param file_type: origin or uploaded
-    :param filename: original file name
-    :return: customized file name
-    """
-    prefix = "images/"
-    file_type = file_type + "/"
-    name = str(uuid4()).replace("-", "")
-    extension = os.path.splitext(filename)[-1]
-    return prefix + file_type + name + extension
+        :param function: main function
+        :return: wrapper
+        """
+
+        def wrapper(*args, **kwargs):
+            """
+
+            :param args:
+            :param kwargs:
+            :return: result of main function
+            """
+
+            self = args[0]
+
+            result = UploadedImage.objects.filter(pk=self.pk)
+            previous = result[0] if len(result) else None
+            super(UploadedImage, self).save()
+
+            result = function(*args, **args)
+
+            if previous:
+                os.remove(settings.MEDIA_ROOT + "/uploaded/" + previous.image.name)
+            return result
+
+        return wrapper
+
+    @delete_previous_file
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
+        super(UploadedImage, self).save()
+
+    @delete_previous_file
+    def delete(self, using=None, keep_parents=False):
+        super(UploadedImage, self).delete()
+
+    def get_image_path(filename):
+        """
+        To Get new customized file path
+        :param filename: original file name
+        :return: customized file name
+        """
+        prefix = "images/"
+        file_type = "uploaded/"
+        name = str(uuid4()).replace("-", "")
+        extension = os.path.splitext(filename)[-1]
+        return prefix + file_type + name + extension
+
+
