5d04aa6b9202569a2a53c8c9ffffa1f9739d2cf7
Shimbo Takaaki <ta7uwtaka@gmail.com>
2018-07-18 23:48:10 +0900

    Add: staticfiles dir and static root

--- settings/settings.py
+++ settings/settings.py
@@ -115,6 +115,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIR = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = [os.path.join(BASE_DIR, "static")]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "static/images")
 MEDIA_URL = "/static/images/"
